107153a6297ea72955edd522a7ad930329c4c1ff
Leeyoungmin1 <163329389+Leeyoungmin1@users.noreply.github.com>
2025-04-22 15:03:59 +0900

    모바일 대응 완료

--- src/components/FireMessage.jsx
+++ src/components/FireMessage.jsx
@@ -1,3 +1,5 @@
+import './Message.css';
+
 export default function FireMessage() {
   const handleClick = () => {
     window.open('https://example.com', '_blank'); // 링크 이동
@@ -6,13 +8,13 @@ export default function FireMessage() {
   return (
     <div
       onClick={handleClick}
-      className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 cursor-pointer hover:opacity-90 transition"
+      className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 cursor-pointer hover:opacity-90 transition message-style"
       style={{
         backgroundColor: 'rgba(0, 102, 204, 0.85)', // 파란 배경
         color: 'white',
-        fontSize: '24px',
+        // fontSize: '24px',
         padding: '16px 28px',
-        borderRadius: '99px',
+        // borderRadius: '99px',
         boxShadow: '0 8px 12px rgba(0,0,0,0.6)',
         top: '17%',
       }}
--- src/components/ToastMessage.jsx
+++ src/components/ToastMessage.jsx
@@ -1,15 +1,17 @@
+import './Message.css';
+
 export default function ToastMessage({ message, onClick }) {
   return (
     <div
       onClick={onClick}
-      className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 cursor-pointer"
+      className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 cursor-pointer message-style"
       style={{
         backgroundColor: 'rgba(221, 1, 1, 0.85)',
         color: 'white',
         top: '17%',
-        fontSize: '24px',
+        // fontSize: '24px',
         padding: '16px 28px',
-        borderRadius: '99px',
+        // borderRadius: '99px',
         boxShadow: '0 8px 12px rgba(0,0,0,0.6)',
         transition: 'all 0.3s',
       }}
